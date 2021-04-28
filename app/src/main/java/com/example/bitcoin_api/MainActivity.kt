4e26fd874ac31991191f681530264eae0a0ec799
Diego Lins <68483773+DiegoLins10@users.noreply.github.com>
2021-04-28 10:04:14 -0300

    config gradle 28/04

--- app/src/main/java/com/example/bitcoin_api/MainActivity.kt
+++ app/src/main/java/com/example/bitcoin_api/MainActivity.kt
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    val API_URL = "https://www.mercadobitcoin.net/api/BTC/ticker/"
+
+    var cotacaoBitcoin: Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buscaCotacao()
     }
 }
