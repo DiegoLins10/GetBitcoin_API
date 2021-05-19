372b8c13db1dfdeb0cd96ecbcbca18db3d188bc4
Diego Lins <68483773+DiegoLins10@users.noreply.github.com>
2021-05-18 21:19:14 -0300

    atividade update

--- app/src/main/java/com/example/bitcoin_api/MainActivity.kt
+++ app/src/main/java/com/example/bitcoin_api/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.example.bitcoin_api
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-import java.net.URL
-import android.util.Log
-import org.jetbrains.anko.alert
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.bloco_cotacao.*
+import kotlinx.android.synthetic.main.bloco_data.*
+import kotlinx.android.synthetic.main.bloco_entrada.*
+import kotlinx.android.synthetic.main.bloco_maior.*
+import kotlinx.android.synthetic.main.bloco_menor.*
+import kotlinx.android.synthetic.main.bloco_saida.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 import org.json.JSONObject
-import java.lang.RuntimeException
-
-import kotlinx.android.synthetic.main.bloco_cotacao.*
-import kotlinx.android.synthetic.main.bloco_entrada.*
-import kotlinx.android.synthetic.main.bloco_saida.*
+import java.net.URL
 import java.text.NumberFormat
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +21,9 @@ class MainActivity : AppCompatActivity() {
     val API_URL = "https://www.mercadobitcoin.net/api/BTC/ticker/"
 
     var cotacaoBitcoin: Double = 0.0
+    var maiorBitcoin: Double = 0.0
+    var menorBitcoin: Double = 0.0
+    var dataBitcoin: Long = 0
     private val TAG = "Fatec Ferraz"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,12 +43,24 @@ class MainActivity : AppCompatActivity() {
             //acessar API e buscar seu resultado
             val resposta = URL(API_URL).readText()
             cotacaoBitcoin = JSONObject(resposta).getJSONObject("ticker").getDouble("last")
+            maiorBitcoin = JSONObject(resposta).getJSONObject("ticker").getDouble("high")
+            menorBitcoin = JSONObject(resposta).getJSONObject("ticker").getDouble("low")
+            dataBitcoin = JSONObject(resposta).getJSONObject("ticker").getLong("date")
             val f = NumberFormat.getCurrencyInstance(Locale("pt", "br"))
+            val date: Date = Date(dataBitcoin * 1000L)
+            val formato = "dd/MM/yyyy HH:mm:ss"
+            val format = SimpleDateFormat(formato)
+            val dataFormatada = format.format(date)
             val cotacaoFormatada = f.format(cotacaoBitcoin)
+            val maiorFormatada = f.format(maiorBitcoin)
+            val menorFormatada = f.format(maiorBitcoin)
             uiThread {
                // alert ("$cotacaoBitcoin").show()
                // alert ("$cotacaoFormatada").show()
-            txtCotacao.setText(" $cotacaoFormatada")
+            txtCotacao.setText("$cotacaoFormatada")
+            txtMaior.setText("$maiorFormatada")
+            txtMenor.setText("$menorFormatada")
+            txtData.setText("$dataFormatada")
             }
         }
     }
@@ -57,7 +71,7 @@ class MainActivity : AppCompatActivity() {
         }
         //valor digitado usuario
         val valorDigitado = txtValor.text.toString()
-            .replace(",",".").toDouble()
+            .replace(",", ".").toDouble()
             //.replace(".","").toDouble().toDouble()
         val resultado = if(cotacaoBitcoin>0) valorDigitado / cotacaoBitcoin
         else 0.0
